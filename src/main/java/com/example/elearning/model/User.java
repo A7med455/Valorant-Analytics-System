888f0f51223a3ca09471168e40e8d232825882d8
saraamohamedd <sararabbasy@gmail.com>
2026-05-02 22:54:34 +0200

    user model updated

--- src/main/java/com/example/elearning/model/User.java
+++ src/main/java/com/example/elearning/model/User.java
@@ -4,39 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table (name="users")
 public class User {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long Id;
 
-private long Id;
+@Column(unique = true)
 private String email;
 private String password;
+
 private String fname;
 private String lname;
 private String role;
 private Boolean status;
 
 //a user can enroll in many courses
-    @OneToMany(mappedBy = "User")
+@OneToMany(mappedBy = "user")
     private List<Enrollment> enrollment=new ArrayList<>();
+
 //a user can have many courses
-@OneToMany(mappedBy = "User")
-private List <Course> courses=new ArrayList<>();
+@OneToMany(mappedBy = "user")
+private List <Course> createdcourses=new ArrayList<>();
 
 //each user have one wallet
-    @OneToOne(mappedBy = "User")
+@OneToOne(mappedBy = "user")
+private Wallet wallet;
 
 public User(){
-}
-
-public User(long Id,String email,String password, String fname, String lname,String role,Boolean status){
-    this.Id=Id;
-    this.email=email;
-    this.password=password;
-    this.fname=fname;
-    this.lname=lname;
-    this.role=role;
-    this.status=status;
 }
 
 public String getEmail(){
@@ -51,34 +46,42 @@ public void setEmail(String email){
     public void setPassword(String password){
         this.password=password;
     }
-    public long getId(){
+
+    public Long getId(){
         return Id;
     }
-    public void setId(long Id){
+    public void setId(Long Id){
         this.Id=Id;
     }
+
     public String getFname(){
         return fname;
     }
     public void setFname(String fname){
         this.fname=fname;
     }
+
     public String getLname(){
         return lname;
     }
     public void setLname(String lname){
         this.lname=lname;
     }
+
     public String getRole(){
         return role;
     }
     public void setRole(String role){
         this.role=role;
     }
+
     public Boolean getStatus(){
         return status;
     }
     public void setStatus(Boolean status){
         this.status=status;
     }
+
+    public Wallet getWallet(){return wallet;}
+    public void setWallet(Wallet wallet){this.wallet=wallet;}
 }

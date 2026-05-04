43d8ace8fc90d17a639f76bd7321665fffd822bc
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-05-04 21:09:14 +0300

    Card Model Class Added

---
+++ src/main/java/com/example/elearning/model/Card.java
@@ -0,0 +1,100 @@
+package com.example.elearning.model;
+
+import jakarta.persistence.*;
+
+@Entity // tells JPA this is a DB table called card
+@Table(name="card")
+public class Card
+{
+    @Id //auto increment id (PK)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long cardId;
+
+    //Each user can have one card
+    @OneToOne
+    @JoinColumn(name="user_id",unique = true)
+    private User user;
+
+    private String cardHolderName;
+    //store the last 4 digits only for security purposes
+    private String lastFourDigits;
+    private String expiryMonth;
+    private String expiryYear;
+    private String cvv;
+
+    //Required by JPA to create objects
+    public Card()
+    {}
+
+    public Long getCardId()
+    {
+        return cardId;
+    }
+
+    public void setCardId(Long cardId)
+    {
+        this.cardId = cardId;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+
+    public String getCardHolderName()
+    {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName)
+    {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getLastFourDigits()
+    {
+        return lastFourDigits;
+    }
+
+    public void setLastFourDigits(String lastFourDigits)
+    {
+        this.lastFourDigits = lastFourDigits;
+    }
+
+    public String getExpiryMonth()
+    {
+        return expiryMonth;
+    }
+
+    public void setExpiryMonth(String expiryMonth)
+    {
+        this.expiryMonth = expiryMonth;
+    }
+
+    public String getExpiryYear()
+    {
+        return expiryYear;
+    }
+
+    public void setExpiryYear(String expiryYear)
+    {
+        this.expiryYear = expiryYear;
+    }
+
+    public String getCvv()
+    {
+        return cvv;
+    }
+
+    public void setCvv(String cvv)
+    {
+        this.cvv = cvv;
+    }
+
+
+}
